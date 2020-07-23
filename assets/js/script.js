818305c5928088464920f278a2408f35ac15adc6
majoya040 <34689250+majoya040@users.noreply.github.com>
2020-07-24 02:48:53 +0600

    around the world

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,14 @@ const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 let i = 0;
 
 Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
-  item.style.cssText = `background-color: ${colors[i++]}`;
+  item.style.cssText = `background-color: ${colors[i++]
+  }`;
 });
   
   Array.from(document.querySelectorAll(".nav-button")).forEach((item)=>{ item.onclick=() =>{
-    item.parentElement.parentElement.classList.toggle("change")
+    item.parentElement.parentElement.classList.toggle("change");
 };
-}); 
+
+});
+
+
